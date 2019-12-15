090120adb47fa0fbb402ec8eaf837cb2078ead8a
Thomas HENON <tounefr@users.noreply.github.com>
2019-12-15 18:52:15 +0100

    Update guest-create.go

--- esxi/guest-create.go
+++ esxi/guest-create.go
@@ -238,7 +238,7 @@ func guestCREATE(c *Config, guest_name string, disk_store string,
 		}
 
 		ovf_cmd := fmt.Sprintf("ovftool --acceptAllEulas --noSSLVerify --X:useMacNaming=false %s "+
-			"-dm=%s --name='%s' --overwrite -ds=%s' %s '%s' '%s'", extra_params, boot_disk_type, guest_name, disk_store, net_param, src_path, dst_path)
+			"-dm=%s --name='%s' --overwrite -ds='%s' %s '%s' '%s'", extra_params, boot_disk_type, guest_name, disk_store, net_param, src_path, dst_path)
 
 		if runtime.GOOS == "windows" {
 			osShellCmd = "cmd.exe"
